9858f645ce97a069c35aadc4528e19c99570091d
Nikita Pyresev <wegofender@gmail.com>
2020-08-03 21:40:02 +0300

    classwork variables data types

---
+++ classwork1/main.js
@@ -0,0 +1,70 @@
+// variables
+
+// 1. let
+// 2. const
+// 3. var - устаревший вариант
+
+// <let or const> <variableNameThayIGiveToIt> = <content>
+
+/*
+// объявление переменной
+let userName = "Danya";
+
+// объявление переменной
+let points = 5;
+
+// объявление переменной
+const game = "Pinball";
+// game = "Dota"; <--- так делаьб нельзя, будет ошибка
+
+// вовод переменной в консоль
+console.log(game);
+
+// вывод нескольких переменных в консоль
+console.log(userName, game, points);
+
+// запись значения в переменню
+userName = "Nikita";
+
+// вовод переменной в консоль
+console.log(userName);
+
+// запись значения в переменню
+userName = "Sasha";
+*/
+
+/*
+// вывод в консоль вычисляемого выражения
+console.log(2 + 2);
+
+// вычисление выражения в консоли
+const result = 3 + 5 * 8 + (10 + 45);
+console.log(result);
+*/
+
+/*
+let a = 5;
+let b = 6;
+let c = a + b; // 11
+let d = c; // 11
+c = 2 * 2;
+// d = 11
+*/
+
+/*
+let a = "danya";
+let b = "nikita";
+// записали значение переменной b в переменную а
+a = b; // a = "nikita"
+// записали значение переменной a в переменную b
+b = a; // b = "nikita"
+console.log(b);
+*/
+
+// let a = 3;
+// let b = "3";
+
+
+
+
+
---
+++ classwork1/types.js
@@ -0,0 +1,55 @@
+// Data types
+/*
+// number число
+let a = 1;
+let b = -23529357209;
+let c = Infinity;
+let d = NaN;
+
+// string строка
+let x = "Nikita";
+let z = "Danya";
+let str = "456";
+let fakeBoolean = "false";
+
+// Boolean логический тип данных
+const flag1 = true;
+const flag2 = false;
+
+// undefined
+let hhh;
+// console.log(hhh);
+
+// null
+let money = null;
+// сняли деньги
+money = 150;
+
+// object
+const user = {
+  name: "Sasha",
+  points: 12,
+};
+const names = ["denys", "ruslan"];
+*/
+
+/*
+let j = "";
+let type = typeof j;
+// let type = typeof (j);
+console.log(type);
+
+let type2 = typeof "";
+console.log(type2);
+
+console.log(typeof "");
+*/
+
+let variable = "user";
+let user = "Sasha";
+let name = "nikita";
+
+console.log(user); // Sasha
+console.log("user"); // user
+
+// Приведение (изменение) типов данных
